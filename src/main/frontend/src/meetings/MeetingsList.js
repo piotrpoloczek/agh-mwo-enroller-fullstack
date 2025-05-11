48814be36f921b2dfba540101a91c4aceb99b7b6
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-05-11 11:13:36 +0200

    add the logic for deleting data

--- src/main/frontend/src/meetings/MeetingsList.js
+++ src/main/frontend/src/meetings/MeetingsList.js
@@ -1,17 +1,21 @@
-export default function MeetingsList({meetings}) {
+export default function MeetingsList(props) {
     return (
         <table>
             <thead>
             <tr>
+                <th>Id</th>
                 <th>Nazwa spotkania</th>
                 <th>Opis</th>
+                <th>Usuń</th>
             </tr>
             </thead>
             <tbody>
             {
-                meetings.map((meeting, index) => <tr key={index}>
+                props.meetings.map((meeting, index) => <tr key={index}>
+                    <td>{meeting.id}</td>
                     <td>{meeting.title}</td>
                     <td>{meeting.description}</td>
+                    <td><button onClick={() => props.onDelete(meeting)}>Usuń</button></td>
                 </tr>)
             }
             </tbody>
--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -15,7 +15,8 @@ export default function MeetingsPage({username}) {
             }
         };
         fetchMeetings();
-    }, []);
+    }, );
+
 
     async function handleNewMeeting(meeting) {
         const response = await fetch('/api/meetings', {
@@ -30,9 +31,14 @@ export default function MeetingsPage({username}) {
         }
     }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m !== meeting);
+            setMeetings(nextMeetings);
+        }
     }
 
     return (
